4b188965a86022fe686e4e59b2c026377024237d
Yijie Huang <550684610@qq.com>
2024-06-05 20:42:00 +0800

    Used for testing access in oop

--- ScalaBasics/src/main/scala/ClassForAccess.scala
+++ ScalaBasics/src/main/scala/ClassForAccess.scala
@@ -1,3 +1,13 @@
-object ClassForAccess {
+object ClassForAccess extends App{
+
+  class Person {
+    private var ID: String = "510131"
+    protected var name: String = "Jon"
+    var sex: String = "Male"
+
+    def printInfo(): Unit = {
+      println(s"$name 's id is $ID")
+    }
+  }
 
 }
