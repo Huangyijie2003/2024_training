fbc5d2ba0b013e881fb037840ff7cc880125beb5
Yijie Huang <550684610@qq.com>
2024-06-06 16:19:40 +0800

    Finish OOP code

--- ScalaBasics/src/main/scala/OOP.scala
+++ ScalaBasics/src/main/scala/OOP.scala
@@ -98,4 +98,155 @@ object OOP extends App {
 
   person2.sleep()
   person2.eat()
+
+  class Student3 private(val name: String, val age: Int){
+    def printInfo(): Unit = {
+      println(s"Name: $name, age: $age, school: ${Student3.school}")
+    }
+  }
+
+  object Student3 {
+    var school: String = "CUG" // 伴生对象里的属性可以当成静态属性来用
+
+    def apply(name: String, age: Int): Student3 = new Student3(name, age)
+  }
+
+  val student3 = Student3("Jon", 21)
+
+  student3.printInfo()
+
+  class Singleton private() {
+    def doSomething(): Unit = {
+      println("Doing something in a more controlled singleton instance...")
+    }
+  }
+
+  // 饿汉式单例设计模式
+  object Singleton {
+    private val instance = new Singleton()
+
+    def getInstance: Singleton = instance
+  }
+
+  // 懒汉式单例设计模式
+//  object Singleton {
+//    private var instance: Singleton = _
+//
+//    def getInstance(): Singleton = {
+//      if (instance == null){
+//        instance = new Singleton()
+//      }
+//      instance
+//    }
+//
+//  }
+
+  val singleton = Singleton.getInstance
+  singleton.doSomething() // 输出 "Doing something in a more controlled singleton instance..."
+
+  val singleton2 = Singleton.getInstance
+  singleton2.doSomething()
+
+  println(singleton)
+  println(singleton2)
+
+  class Person4 {
+    val name: String = "person"
+    var age: Int = 18
+    def sayHello():Unit = {
+      println("Hello from: " + name)
+    }
+  }
+
+  // 定义一个特质
+  trait Young {
+    // 定义一个抽象属性
+    var age: Int
+    // 定义一个非抽象属性
+    val name: String = "Young"
+
+    // 声明非抽象方法
+    def play(): Unit = {
+      println(s"Young people $name is playing")
+    }
+    // 声明抽象方法
+    def dating(): Unit
+  }
+
+  trait Knowledge {
+    var amount: Int = 0
+
+    def increase(): Unit
+  }
+
+  trait Talent {
+    def singing(): Unit
+    def dancing(): Unit
+  }
+
+  class Student4 extends Person4 with Young with Knowledge {
+    // 重写冲突的属性name, 不然会报错
+    override val name: String = "Jon"
+    // 实现抽象方法
+    override def dating(): Unit = {
+      println(s"Student $name is dating")
+    }
+
+    override def increase(): Unit = {
+      amount += 1
+      println(s"$name's current amount is $amount")
+    }
+    // 定义类特有的方法
+    def study(): Unit = {
+      println(s"$name is studying")
+    }
+
+    //重写父类方法
+    override def sayHello(): Unit = {
+      super.sayHello()
+      println(s"Hello from student $name")
+    }
+  }
+
+  val student4 = new Student4
+  student4.sayHello()
+  student4.study()
+  student4.play()
+  student4.dating()
+
+  student4.increase()
+  student4.increase()
+  student4.increase()
+
+  val studentWithTalent = new Student4 with Talent {
+    override def singing(): Unit = {
+      println(s"$name is singing")
+    }
+
+    override def dancing(): Unit = {
+      println(s"$name is dancing")
+    }
+  }
+
+  studentWithTalent.singing()
+
+  class User(val name: String, val password: String)
+
+  trait UserDao {
+    _: User =>
+
+    def insert(): Unit = {
+      println(s"Insert into database: ${this.name}")
+    }
+  }
+
+  class RegisterUser(name:String, password:String) extends User(name, password) with UserDao
+
+  val user = new RegisterUser(name="Jon", password="123456")
+
+  user.insert()
+
+
+
+
 }
